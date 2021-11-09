c127cfc1138e0a9c858f03367744a8f83437e934
abubaker <mohammad_abubaker@yahoo.com>
2021-11-10 00:43:34 +0500

    Working...

--- app/src/main/java/org/abubaker/favdish/SplashActivity.kt
+++ app/src/main/java/org/abubaker/favdish/SplashActivity.kt
@@ -2,10 +2,19 @@ package org.abubaker.favdish
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.abubaker.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // Setup ViewBinding for our activity
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        splashBinding.tvAppName.text = "Hello World"
+
     }
 }
