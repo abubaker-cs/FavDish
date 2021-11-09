e905c32e204878e876328512c1d5bf6b063f15bd
abubaker <mohammad_abubaker@yahoo.com>
2021-11-10 01:03:57 +0500

    Working...

--- app/src/main/java/org/abubaker/favdish/SplashActivity.kt
+++ app/src/main/java/org/abubaker/favdish/SplashActivity.kt
@@ -1,6 +1,9 @@
 package org.abubaker.favdish
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import org.abubaker.favdish.databinding.ActivitySplashBinding
 
@@ -13,6 +16,22 @@ class SplashActivity : AppCompatActivity() {
         // Setup ViewBinding for our activity
         val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
+
+        // Full screen for newer versions SDK >= API 30
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+
+            // Ignore depreciated functions
+            // Useful for older versions
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+
+        }
+
 
         splashBinding.tvAppName.text = "Hello World"
 
