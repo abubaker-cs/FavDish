05d997fd377ca3d7be74aff73e8c8bdc056e7ae1
abubaker <mohammad_abubaker@yahoo.com>
2021-11-10 20:19:02 +0500

    Organizing files structure for MVVM Development

---
+++ app/src/main/java/org/abubaker/favdish/view/activities/AddUpdateDishActivity.kt
@@ -0,0 +1,12 @@
+package org.abubaker.favdish.view.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.abubaker.favdish.R
+
+class AddUpdateDishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update_dish)
+    }
+}
