9673189196d1c8ed2767c11deb997f97054f4dda
abubaker <mohammad_abubaker@yahoo.com>
2021-11-10 21:24:54 +0500

    Working...

--- app/src/main/java/org/abubaker/favdish/view/fragments/AllDishesFragment.kt
+++ app/src/main/java/org/abubaker/favdish/view/fragments/AllDishesFragment.kt
@@ -8,30 +8,37 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import org.abubaker.favdish.databinding.FragmentHomeBinding
+import org.abubaker.favdish.databinding.FragmentAllDishesBinding
 import org.abubaker.favdish.viewModel.HomeViewModel
 
 class AllDishesFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentAllDishesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentAllDishesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
+
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
