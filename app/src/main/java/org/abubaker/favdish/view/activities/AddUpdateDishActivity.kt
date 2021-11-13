70f48f8398844d73759d6f216df0b768384caf1a
abubaker <mohammad_abubaker@yahoo.com>
2021-11-13 11:17:19 +0500

    Implemented base functionality for the "custom dialog" that will be displayed once the user will click on the "Add Dish Image" icon.

--- app/src/main/java/org/abubaker/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/org/abubaker/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,13 @@
 package org.abubaker.favdish.view.activities
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import org.abubaker.favdish.R
 import org.abubaker.favdish.databinding.ActivityAddUpdateDishBinding
+import org.abubaker.favdish.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity(),
     View.OnClickListener {
@@ -44,11 +46,9 @@ class AddUpdateDishActivity : AppCompatActivity(),
                 R.id.iv_add_dish_image -> {
 
                     // Display the Toast Message
-                    Toast.makeText(
-                        this@AddUpdateDishActivity,
-                        "You have clicked on the ImageView.",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    // Toast.makeText(this@AddUpdateDishActivity, "You have clicked on the ImageView.", Toast.LENGTH_SHORT).show()
+
+                    customImageSelectionDialog()
 
                     //Get back
                     return
@@ -57,6 +57,45 @@ class AddUpdateDishActivity : AppCompatActivity(),
             }
 
         }
+    }
+
+    /**
+     * A function to launch the custom image selection dialog.
+     */
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this@AddUpdateDishActivity)
+
+        // Inflate the layout: dialog_custom_image_selection_binding.xml
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        // onClick: Camera
+        binding.tvCamera.setOnClickListener {
+            Toast.makeText(
+                this@AddUpdateDishActivity,
+                "You have clicked on the Camera.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            // Close the Dialog after displaying the Toast Message
+            dialog.dismiss()
+        }
+
+        // onClick: Gallery
+        binding.tvGallery.setOnClickListener {
+            Toast.makeText(
+                this@AddUpdateDishActivity,
+                "You have clicked on the Gallery.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            // Close the Dialog after displaying the Toast Message
+            dialog.dismiss()
+        }
+
+        //Start the dialog and display it on screen.
+        dialog.show()
     }
 
 
