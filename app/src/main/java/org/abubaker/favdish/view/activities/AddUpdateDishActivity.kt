81e131828fbfbb11c6c216ac5bd09fa6eb18d9e9
abubaker <mohammad_abubaker@yahoo.com>
2021-11-13 10:39:06 +0500

    Working on the ActionBar for activity_add_update_dish.xml

--- app/src/main/java/org/abubaker/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/org/abubaker/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,10 +1,14 @@
 package org.abubaker.favdish.view.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import org.abubaker.favdish.R
 import org.abubaker.favdish.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(),
+    View.OnClickListener {
 
     private lateinit var mBinding: ActivityAddUpdateDishBinding
 
@@ -19,7 +23,31 @@ class AddUpdateDishActivity : AppCompatActivity() {
         // Initialize the ActionBar with required actions for the onClick Event
         setupActionBar()
 
+        // Assign the click event to the image button.
+        mBinding.ivAddDishImage.setOnClickListener(this@AddUpdateDishActivity)
+
     }
+
+
+    // Action for: Add Image (icon)
+    override fun onClick(v: View?) {
+
+        // Perform the action when user clicks on the addDishImage and show Toast message for now.
+        when (v.id) {
+
+            R.id.iv_add_dish_image -> {
+
+                Toast.makeText(
+                    this@AddUpdateDishActivity,
+                    "You have clicked on the ImageView.",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                return
+            }
+        }
+    }
+
 
     private fun setupActionBar() {
 
