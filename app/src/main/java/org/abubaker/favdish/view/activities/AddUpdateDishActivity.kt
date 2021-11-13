b72260c17cd59b231be37532b471077f38f322b5
abubaker <mohammad_abubaker@yahoo.com>
2021-11-13 09:48:34 +0500

    Working on the ActionBar for activity_add_update_dish.xml

--- app/src/main/java/org/abubaker/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/org/abubaker/favdish/view/activities/AddUpdateDishActivity.kt
@@ -2,11 +2,37 @@ package org.abubaker.favdish.view.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import org.abubaker.favdish.R
+import org.abubaker.favdish.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+
+        // Inflate Layout: activity_add_update_dish.xml
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        // Initialize the ActionBar with required actions for the onClick Event
+        setupActionBar()
+
     }
+
+    private fun setupActionBar() {
+
+        // Enable Support for the ActionBar
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Assign required action on the click event
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+    }
+
+
 }
