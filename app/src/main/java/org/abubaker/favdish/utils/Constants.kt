35ab0e6ec9ad5aa519f110234af2df630c7a93eb
abubaker <mohammad_abubaker@yahoo.com>
2021-11-20 17:39:58 +0500

    Creating Constants.kt for added details such as Dist Type, Category and Cooking Time

---
+++ app/src/main/java/org/abubaker/favdish/utils/Constants.kt
@@ -0,0 +1,90 @@
+package org.abubaker.favdish.utils
+
+/**
+ * This is used to define the constant values that we can use throughout the application.
+ */
+object Constants {
+
+    const val DISH_TYPE: String = "DishType"
+    const val DISH_CATEGORY: String = "DishCategory"
+    const val DISH_COOKING_TIME: String = "DishCookingTime"
+
+    /**
+     * Dish Type
+     */
+    fun dishTypes(): ArrayList<String> {
+
+        // Define List
+        val list = ArrayList<String>()
+
+        // Add Items
+        list.add("breakfast")
+        list.add("lunch")
+        list.add("snacks")
+        list.add("dinner")
+        list.add("salad")
+        list.add("side dish")
+        list.add("dessert")
+        list.add("other")
+
+        // Return List
+        return list
+
+    }
+
+    /**
+     *  Dish Category
+     */
+    fun dishCategories(): ArrayList<String> {
+
+        // Define List
+        val list = ArrayList<String>()
+
+        // Add Items
+        list.add("Pizza")
+        list.add("BBQ")
+        list.add("Bakery")
+        list.add("Burger")
+        list.add("Cafe")
+        list.add("Chicken")
+        list.add("Dessert")
+        list.add("Drinks")
+        list.add("Hot Dogs")
+        list.add("Juices")
+        list.add("Sandwich")
+        list.add("Tea & Coffee")
+        list.add("Wraps")
+        list.add("Other")
+
+        // Return List
+        return list
+
+    }
+
+
+    /**
+     *  Dish Cooking Time
+     */
+    fun dishCookTime(): ArrayList<String> {
+
+        // Define the List
+        val list = ArrayList<String>()
+
+        // Add items
+        list.add("10")
+        list.add("15")
+        list.add("20")
+        list.add("30")
+        list.add("45")
+        list.add("50")
+        list.add("60")
+        list.add("90")
+        list.add("120")
+        list.add("150")
+        list.add("180")
+
+        // Return List
+        return list
+    }
+
+}
