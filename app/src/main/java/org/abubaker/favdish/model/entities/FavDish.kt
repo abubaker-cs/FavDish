6a5767245a28d739ac1203b32ba129ad6a000b74
abubaker <mohammad_abubaker@yahoo.com>
2021-11-23 12:51:38 +0500

    Setting up the Entity: FavDish.kt (Structure for the Table inside the ROOM DB)

--- app/src/main/java/org/abubaker/favdish/model/entities/FavDish.kt
+++ app/src/main/java/org/abubaker/favdish/model/entities/FavDish.kt
@@ -9,20 +9,43 @@ import androidx.room.PrimaryKey
  * create data class with the entities (Dish Items) that we want to insert in the database.
  */
 
-// Define the Table name
+// Define the Table name using @Entity() and set the class to be a "data class"
 @Entity(tableName = "fav_dishes_table")
 data class FavDish(
+
+    // Image
     @ColumnInfo val image: String,
-    @ColumnInfo val imageSource: String, // Local or Online
+
+    // Image Source: Local or Online
+    @ColumnInfo val imageSource: String,
+
+    // Title
     @ColumnInfo val title: String,
+
+    // Type
     @ColumnInfo val type: String,
+
+    // Category
     @ColumnInfo val category: String,
+
+    // Ingredients
     @ColumnInfo val ingredients: String,
 
-    // Specifies the name of the column in the table if you want it to be different from the name of the member variable.
+    /**
+     * Specifies the name of the column in the table
+     * if you want it to be different from the name of the member variable.
+     */
+
+    // Cooking Time
     @ColumnInfo(name = "cookingTime") val cooking_time: String,
+
+    // Instructions
     @ColumnInfo(name = "instructions") val direction_to_cook: String,
+
+    // Favorite Dish
     @ColumnInfo(name = "favoriteDish") var favorite_dish: Boolean = false,
 
+    // PrimaryKey
     @PrimaryKey(autoGenerate = true) val id: Int = 0
+
 )
