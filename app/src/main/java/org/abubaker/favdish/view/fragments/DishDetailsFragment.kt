46296836f2021a7548a1108e522f62275f48499a
abubaker <mohammad_abubaker@yahoo.com>
2021-12-01 10:35:46 +0500

    Setting up dependencies for SafeArgs

---
+++ app/src/main/java/org/abubaker/favdish/view/fragments/DishDetailsFragment.kt
@@ -0,0 +1,20 @@
+package org.abubaker.favdish.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.abubaker.favdish.R
+
+class DishDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dish_details, container, false)
+    }
+
+}
