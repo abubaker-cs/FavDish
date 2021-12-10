19f3580d819cba6622fbd542a7204382fbe23944
abubaker <mohammad_abubaker@yahoo.com>
2021-12-10 23:49:03 +0500

    Starting to work on the Random Dish feature using Retrofit/RxJava

--- app/src/main/java/org/abubaker/favdish/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/org/abubaker/favdish/view/fragments/RandomDishFragment.kt
@@ -4,44 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import org.abubaker.favdish.databinding.FragmentRandomDishBinding
-import org.abubaker.favdish.viewModel.RandomDishViewModel
 
 class RandomDishFragment : Fragment() {
-
-    private lateinit var randomDishViewModel: RandomDishViewModel
-    private var _binding: FragmentRandomDishBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    
+    private var binding: FragmentRandomDishBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        randomDishViewModel =
-            ViewModelProvider(this)[RandomDishViewModel::class.java]
 
-        _binding = FragmentRandomDishBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        // Initialize the ViewBinding.
+        binding = FragmentRandomDishBinding.inflate(inflater, container, false)
+        return binding!!.root
 
-        val textView: TextView = binding.textNotifications
-
-        randomDishViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-
-        return root
     }
 
+    // Override the onDestroy method and make the ViewBinding null when it is called.
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        binding = null
     }
 }
